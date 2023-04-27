76f01a07170224dd61d2829397aec73942423428
Juan Pablo Peña F <pablo27-99@hotmail.com>
2023-04-26 21:59:33 -0500

    extra spaces and console log deleted

--- test/app.test.js
+++ test/app.test.js
@@ -5,26 +5,24 @@ test("Test to verify that it generates passwords with numbers", () => {
     let uppercase = undefined
     let symbols = undefined
     let password = generatePassword(numbers, uppercase, symbols)
-    
     const containsNumber = /\d/.test(password);
     expect(containsNumber).toBe(true);
 });
 
 test("Test to verify that it generates passwords with Uppercase", () => {
     let numbers =undefined
-    let uppercase ='on' 
+    let uppercase ='on'
     let symbols = undefined
     let password = generatePassword(numbers, uppercase, symbols)
     const containsUppercase = /[A-Z]{2,}/g.test(password);
     expect(containsUppercase).toBe(true);
 });
-  
+
 test("Test to verify that it generates passwords with Symbols", () => {
     let numbers =undefined
     let uppercase = undefined
-    let symbols = 'on' 
+    let symbols = 'on'
     let password = generatePassword(numbers, uppercase, symbols)
-    console.log(password)
     const containsSymbols =/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(password);
     expect(containsSymbols).toBe(true);
 });
